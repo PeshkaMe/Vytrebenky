0db20cc88dccc63678eb30aa21a3013e8b23d9c2
PeshkaMe <ganusindmitro3@gmail.com>
2026-08-06 13:42:20 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -51,6 +51,56 @@ io.on('connection', (socket) => {
     });
 });
 
+// Просте сховище акаунтів на сервері (у пам'яті)
+const accounts = {}; 
+
+io.on('connection', (socket) => {
+    console.log('Гравець підключився:', socket.id);
+
+    // Реєстрація
+    socket.on('register', ({ username, password, initialData }) => {
+        if (accounts[username]) {
+            socket.emit('auth_error', 'Користувач з таким логіном вже існує!');
+            return;
+        }
+        accounts[username] = {
+            password: password,
+            data: initialData
+        };
+        socket.emit('auth_success', {
+            username: username,
+            data: accounts[username].data,
+            message: `Вітаємо, ${username}! Акаунт успішно створено.`
+        });
+    });
+
+    // Вхід на акаунт
+    socket.on('login', ({ username, password }) => {
+        const user = accounts[username];
+        if (!user || user.password !== password) {
+            socket.emit('auth_error', 'Невірний логін або пароль!');
+            return;
+        }
+        socket.emit('auth_success', {
+            username: username,
+            data: user.data,
+            message: `З поверненням, ${username}!`
+        });
+    });
+
+    // Збереження даних авторизованого гравця
+    socket.on('save_player', ({ name, data }) => {
+        if (accounts[name]) {
+            accounts[name].data = data;
+        }
+    });
+
+    // Чат
+    socket.on('send_chat_message', (data) => {
+        io.emit('chat_message', data);
+    });
+});
+
 // Port підтримка для Render.com (через process.env.PORT)
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
